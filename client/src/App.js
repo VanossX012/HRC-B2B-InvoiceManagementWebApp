de3b2c62383a299f5a122bb1d5d88a3b0a923cf1
Pranshu Roy <103306159+VanossX012@users.noreply.github.com>
2023-08-01 23:41:22 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Components/Header';
+import Datatable from './Components/Datatable';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <div>
+      <Header/> 
+       <Datatable/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
